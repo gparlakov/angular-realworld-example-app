0574c686b2d45412e48bc834df79d73f21d87ecb
Georgi Parlakov <gparlakov@gmail.com>
2019-11-05 18:14:20 +0200

    prepare scuri Install Config tsconfig.json configure jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,8 @@ module.exports = {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
     '@env': '<rootDir>/src/environments/environment',
     '@src/(.*)': '<rootDir>/src/src/$1',
-    '@state/(.*)': '<rootDir>/src/app/state/$1'
+    '@state/(.*)': '<rootDir>/src/app/state/$1',
+    'autoSpy':'<rootDir>/src/app/auto-spy.ts'
   },
   transformIgnorePatterns: ['node_modules/(?!(jest-test))'],
   maxWorkers: 4
